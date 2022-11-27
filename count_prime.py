25db476696bd17a85431829cdb1f20601161a9ce
Usmaelabdureman <uabdureman@gmail.com>
2022-11-27 14:26:39 +0300

    remove unnecesary part from countPrime

--- count_prime.py
+++ count_prime.py
@@ -10,7 +10,6 @@
 # 	return count
 	
 def CountPrime(number):
-    if(number > 40): return number**2 - number + 41
     lst_prime=[2]
     checker=[True]*(number)
     for i in range(2,number):
@@ -19,21 +18,3 @@ def CountPrime(number):
             for j in range(i,number,i):
                 checker[j]=False
     return len(lst_prime)
-        
-
-# def Prime(n):
-#     idx=1
-#     lst=[]
-#     for i in range(2,4):
-#         if(n > i): lst.append(i)
-#     while(True):
-#         minus=6*idx-1
-#         plus=6*idx+1
-#         if minus>n:
-#                 break
-#         lst.append(minus)
-#         if plus < n: lst.append(plus)    
-#         idx+=1
-#     return len(lst)
-
-print(CountPrime(100))
