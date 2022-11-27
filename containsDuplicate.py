e5209cf61e9829e64533b35e8adc722714244fac
Usmaelabdureman <uabdureman@gmail.com>
2022-11-27 15:18:17 +0300

    update contains duplicate solution

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,13 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        arr={}
-        output=[]
+        arr=set()
         for i in nums:
             if i in arr:
-                arr[i]+=1
-            else:
-                arr[i]=1
-        for value in arr.values():
-            if value>=2:
-                output.append(value)
-        return True if len(output)>0 else False
+                return True
+            arr.add(i)
+        return False
