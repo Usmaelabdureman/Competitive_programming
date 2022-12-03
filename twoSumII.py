24bb8d2857190f0789faf29b0f2c8cc8b3bab83d
Usmaelabdureman <uabdureman@gmail.com>
2022-12-03 20:08:05 +0300

    two sum II solution

---
+++ twoSumII.py
@@ -0,0 +1,18 @@
+def twoSum(numbers, target):
+        lft=0
+        rgt=len(numbers)-1
+        while lft<rgt:
+            res=numbers[lft]+numbers[rgt]
+    
+            if res<target:
+                lft+=1
+            elif res>target:
+                rgt-=1
+            else:
+                return [lft+1,rgt+1]
+
+
+numbers = [1,2,4,7,11,15]
+target = 9
+
+print(twoSum(numbers,target))
