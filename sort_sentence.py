7fe613041a8d1cb8991e8d2067320c5cc23cfee3
Usmaelabdureman <uabdureman@gmail.com>
2022-12-08 20:05:51 +0300

    sorting sentence

---
+++ sort_sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words = {}
+        for word in s.split():
+            words[word[-1]] = word[:-1]
+        lst=[]
+        for i in sorted(words):
+            lst.append(words[i])
+        return " ".join(lst)
