87e63b215bde0bca029f600367bbb5f81a91591c
Usmaelabdureman <uabdureman@gmail.com>
2022-12-08 18:55:55 +0300

    add domino piling solution

---
+++ Code_Forces/domino_piling.py
@@ -0,0 +1,4 @@
+def DominoPiling(m,n):
+    return (m*n)//2
+
+print(DominoPiling(4,2))
--- dimlight.py
+++ dimlight.py
@@ -1,21 +1,21 @@
-import turtle
+from turtle import *
 import colorsys
-tr=turtle.Turtle()
-tr.tracer(100)
-tr.bgcolor("black")
-tr.pensize(4)
+
+tracer(100)
+bgcolor("black")
+pensize(4)
 h=0
 
 for i in range(400):
     c=colorsys.hsv_to_rgb(h,1,1)
     h+=0.005
-    tr.color("black")
-    tr.fillcolor(c)
-    tr.begin_fill()
-    tr.rt(46.5)
-    tr.fd(i)
-    tr.circle(20,180)
-    tr.end_fill()
-    tr.circle(i,21)
-    tr.fd(100)
-tr.done()
+    color("black")
+    fillcolor(c)
+    begin_fill()
+    rt(46.5)
+    fd(i)
+    circle(20,180)
+    end_fill()
+    circle(i,21)
+    fd(100)
+done()
