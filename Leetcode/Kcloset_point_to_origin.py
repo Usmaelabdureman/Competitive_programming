53ea58e1a1243e4b7a75d5838a6fd18391e761de
Usmaelabdureman <uabdureman@gmail.com>
2022-12-12 17:06:01 +0300

    K closest point to origin

--- Leetcode/Kcloset_point_to_origin.py
+++ Leetcode/Kcloset_point_to_origin.py
@@ -1,21 +1,6 @@
-import math
-lst=[[1,3],[-2,2]]
-# out=[[]]
-def test(lst):
-    for i in lst[0:len(lst)-1]:
-        for j in lst[1:len(lst)]:
-            if i[0]**2+i[1]**2 < j[0]**2+j[1]**2:
-                temp=i
-                i=j
-                j=temp
-    # return 
-for i in lst:
-    for j in lst:
-        # if i[0]**2+i[1]**2 < j[0]**2+j[1]**2:
-            temp=i
-            i=j
-            j=temp
-    print(temp)
-    print(lst)
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda k:k[0]**2+k[1]**2)
+        return points[:k]
 
 
