bbaacddbdf4ae5cbd6103c8d04a3d4e81ee9f674
Usmaelabdureman <uabdureman@gmail.com>
2022-12-22 10:28:24 +0300

    maximum consecutive ones number three

---
+++ max_consecutive_ones_iii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l,max_cons=0,0
+        for idx,num in enumerate(nums):
+            k-=1-num
+            if k<0:
+                k+=1-nums[l]
+                l+=1
+            else:
+                max_cons=max(max_cons,idx-l+1)
+        return max_cons
