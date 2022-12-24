ab9c902dfa73737d257eeadc7a94608ae2d552ec
Usmaelabdureman <uabdureman@gmail.com>
2022-12-24 18:53:48 +0300

    valid palindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -1,3 +1,5 @@
-s="a, man : yo u 45 ,"
-k=''.join(c for c in s if c.isalnum())
-print(k)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st=''.join(char for char in s if char.isalnum())
+        st=st.lower()
+        return st==st[::-1]
