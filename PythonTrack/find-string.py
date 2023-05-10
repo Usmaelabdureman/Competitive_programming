1b0483ac63513a3da5fc2e93b298409a1ba916ef
Usmaelabdureman <uabdureman@gmail.com>
2023-05-10 12:14:06 +0300

    create find string challenge

---
+++ PythonTrack/find-string.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+    count = 0
+    start = 0
+    while True:
+        start = string.find(sub_string, start) + 1
+        if start > 0:
+            count += 1
+        else:
+            return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
