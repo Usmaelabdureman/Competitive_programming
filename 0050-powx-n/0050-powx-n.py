691a98d60355bf9703a4738b11fc20ca7e022fd0
Usmael Abdurhaman <95717548+Usmaelabdureman@users.noreply.github.com>
2023-06-23 15:56:35 +0000

    Time: 52 ms (25.78%), Space: 16.4 MB (32.66%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x,n):
+            if n==0:return 1
+            if x==0:return 0
+            result=helper(x,n//2)
+            result=result*result
+            return x*result if n%2!=0 else result
+        result=helper(x,abs(n))
+        return result if n>=0 else 1/result
