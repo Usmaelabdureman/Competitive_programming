f2e6456cc0fb47e438fdb252762927728a64503d
Usmael Abdurhaman <uabdureman@gmail.com>
2023-08-14 12:13:12 +0300

    Time: 655 ms (99.17%), Space: 27 MB (61.60%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -1,28 +1,29 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        # Add the first k workers with section id of 0 and 
-        # the last k workers with section id of 1 (without duplication) to pq.
-        pq = []
-        for i in range(candidates):
-            pq.append((costs[i], 0))
-        for i in range(max(candidates, len(costs) - candidates), len(costs)):
-            pq.append((costs[i], 1))
-
-        heapify(pq)
+        # head_workers stores the first k workers.
+        # tail_workers stores at most last k workers without any workers from the first k workers. 
+        head_workers = costs[:candidates]
+        tail_workers = costs[max(candidates, len(costs) - candidates):]
+        heapify(head_workers)
+        heapify(tail_workers)
         
         answer = 0
         next_head, next_tail = candidates, len(costs) - 1 - candidates 
 
-        # Only refill pq if there are workers outside.
         for _ in range(k): 
-            cur_cost, cur_section_id = heappop(pq)
-            answer += cur_cost
-            if next_head <= next_tail: 
-                if cur_section_id == 0:
-                    heappush(pq, (costs[next_head], 0))
+            if not tail_workers or head_workers and head_workers[0] <= tail_workers[0]: 
+                answer += heappop(head_workers)
+
+                # Only refill the queue if there are workers outside the two queues.
+                if next_head <= next_tail: 
+                    heappush(head_workers, costs[next_head])
                     next_head += 1
-                else:
-                    heappush(pq, (costs[next_tail], 1))
+            else: 
+                answer += heappop(tail_workers)
+
+                # Only refill the queue if there are workers outside the two queues.
+                if next_head <= next_tail:  
+                    heappush(tail_workers, costs[next_tail])
                     next_tail -= 1
                     
         return answer
