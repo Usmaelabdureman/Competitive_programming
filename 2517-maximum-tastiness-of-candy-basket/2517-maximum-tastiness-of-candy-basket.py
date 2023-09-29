4960e7da9095522ec0cc5a285304916ec1f3bd23
Usmael Abdurhaman <uabdureman@gmail.com>
2023-09-29 17:03:52 +0300

    Time: 1181 ms (64.66%), Space: 28.3 MB (58.38%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
@@ -6,18 +6,15 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             cntOfCandies, last = 1, price[0]
-            
             for i in range(1, n):
                 if price[i] - last >= mid:
                     cntOfCandies += 1
                     last = price[i]
-            
             if cntOfCandies >= k:
                 ans = mid
                 l = mid + 1
             else:
                 r = mid - 1
-        
         return ans
 
     
