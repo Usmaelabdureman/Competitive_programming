fdbfedea23f32492e3725ecc14fed73967b4b106
Usmael Abdurhaman <uabdureman@gmail.com>
2023-10-25 17:10:24 +0300

    Time: 249 ms (86.90%), Space: 20.5 MB (92.55%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -10,8 +10,6 @@ class Solution:
         for num in nums:
             # Take modulo twice to avoid negative remainders.
             prefixMod = (prefixMod + num % k + k) % k
-            # Add the count of subarrays that have the same remainder as the current
-            # one to cancel out the remainders.
             result += modGroups[prefixMod]
             modGroups[prefixMod] += 1
         return result
