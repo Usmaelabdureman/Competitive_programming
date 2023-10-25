f94a2e3b0e3bbb750f2df4594f3689c5595d56e1
Usmael Abdurhaman <uabdureman@gmail.com>
2023-10-25 17:12:36 +0300

    Time: 255 ms (69.36%), Space: 20.7 MB (71.74%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -3,10 +3,8 @@ class Solution:
         n = len(nums)
         prefixMod = 0
         result = 0
-
         modGroups = [0] * k
         modGroups[0] = 1
-
         for num in nums:
             # Take modulo twice to avoid negative remainders.
             prefixMod = (prefixMod + num % k + k) % k
