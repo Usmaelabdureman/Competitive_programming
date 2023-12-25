eb9bd73eb8b7fd4417d7ee32ecf9fdd2f8276813
Usmael Abdurhaman <uabdureman@gmail.com>
2023-12-25 19:17:00 +0300

    Added solution for Matrix Diagonal Sum

--- A2SV/Array-list-matrices/matrix-diagonal-sum.py
+++ A2SV/Array-list-matrices/matrix-diagonal-sum.py
@@ -2,11 +2,12 @@ class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         ans =0
         n=len(mat)
-        for i in range(n):
-            for j in range(n):
-                if i ==j:
-                    ans+=mat[i][j]
-
+        i=0
+        j=0
+        while i<=n-1 and j<=n-1:
+            ans+=mat[i][j]
+            i+=1
+            j+=1
         i=n-1
         j=0
         while i>=0 and j<=n-1:
