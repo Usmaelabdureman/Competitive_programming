34ba53c874f54df5bf46af680728fb3cda02168b
Usmael Abdurhaman <uabdureman@gmail.com>
2024-01-27 20:08:49 +0300

    Add solution for Subarray Sums Divisible by K

---
+++ A2SV/prefix-sum/leetcode/subarray-sums-divisible-by-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraysDivByK(self, nums, k):
+        n = len(nums)
+        prefixMod = 0
+        result = 0
+        modGroups = [0] * k
+        modGroups[0] = 1
+        for num in nums:
+            # Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k
+            result += modGroups[prefixMod]
+            modGroups[prefixMod] += 1
+        return result
+
+        
+        
+        
