758aa71bd88b294ae0b7e3c7a6d0a3af7d6252d1
Usmael Abdurhaman <uabdureman@gmail.com>
2024-01-27 19:38:06 +0300

    Add solution for BoatsToSavePeople

---
+++ LeetCode/BoatsToSavePeople.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left=0
+        right=len(people)-1
+        result=0
+        while left<=right:
+            result+=1
+            if people[left]+people[right]<=limit:
+                left+=1
+            right-=1
+        return result
