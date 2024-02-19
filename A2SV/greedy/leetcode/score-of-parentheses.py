68d68828c25d2f51cf2835a1a70cd1a4d020eafa
Usmael Abdurhaman <uabdureman@gmail.com>
2024-02-19 18:49:17 +0300

    Add solution for Score of Parentheses

---
+++ A2SV/greedy/leetcode/score-of-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+
+        stack = []
+
+        score = 0
+
+        for char in s:
+            if char == "(":
+                stack.append(score)
+                score = 0
+            
+            elif stack and char == ")":
+                score = stack.pop() + max(2*score,1)
+        return score
