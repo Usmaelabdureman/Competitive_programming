c53c7b7f2ad9f0a3325f4adf3f8e2b87f95f820d
Usmael Abdurhaman <uabdureman@gmail.com>
2024-02-19 10:15:30 +0300

    Add solution for Break a Palindrome

--- A2SV/greedy/leetcode/break-a-palindrome.py
+++ A2SV/greedy/leetcode/break-a-palindrome.py
@@ -5,11 +5,7 @@ class Solution:
         
         if len(palindrome) == 1: return ""
 
-        # if len(set(palindrome)) == 1 and len(palindrome) > 1 or palindrome[-1] == 'a': 
-        #     palindrome[-1] = 'b'
-
-        print(set(palindrome))
-
+    
         left = 0
         right = len(palindrome) - 1
 
