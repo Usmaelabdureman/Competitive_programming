a7e75e6a43abfed8bade8db81eac8fce40cee548
Usmael Abdurhaman <uabdureman@gmail.com>
2024-02-20 22:14:28 +0300

    Add solution for Max Increase to Keep City Skyline

--- A2SV/recursion/leetcode/max-increase-to-keep-city-skyline.py
+++ A2SV/recursion/leetcode/max-increase-to-keep-city-skyline.py
@@ -1,8 +1,6 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        row_maxes = [max(row) for row in grid]
-        col_maxes = [max(col) for col in zip(*grid)]
-
-        return sum(min(row_maxes[r], col_maxes[c]) - val
-                   for r, row in enumerate(grid)
-                   for c, val in enumerate(row))
+        maxValInRows = [max(row) for row in grid]
+        maxValInCols = [max(col) for col in zip(*grid)]
+        
+        return sum(min(maxValInRows[r], maxValInCols[c]) - val for r, row in enumerate(grid) for c, val in enumerate(row))
