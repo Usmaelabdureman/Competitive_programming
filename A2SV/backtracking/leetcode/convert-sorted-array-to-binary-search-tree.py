c288baaaf84e01f4c5c180b875678b31ed927844
Usmael Abdurhaman <uabdureman@gmail.com>
2024-02-29 16:05:00 +0300

    Add solution for Convert Sorted Array to Binary Search Tree

---
+++ A2SV/backtracking/leetcode/convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        n = len(nums)
+        if n == 0:
+            return None
+        root = nums[n//2]
+
+        return TreeNode(root,self.sortedArrayToBST(nums[:n//2]), self.sortedArrayToBST(nums[n//2+1:]))
+
+
+
