f5116a2291c2a05b27bad548c0c87870786d96a1
Usmael Abdurhaman <uabdureman@gmail.com>
2024-03-03 19:47:32 +0300

    Add solution for N-Queens

--- A2SV/backtracking/leetcode/n-queens.py
+++ A2SV/backtracking/leetcode/n-queens.py
@@ -1,45 +1,27 @@
 class Solution:
+
+    def isValid(self,row,col,path):
+        for prev_row, prev_corr in enumerate(path):
+                prev_col = prev_corr.index('Q')
+                if prev_col == col or prev_row - prev_col == row - col or prev_row + prev_col == row + col:
+                    return False
+        return True
+
     def solveNQueens(self, n: int) -> List[List[str]]:
-        solutions = []
-        state = []
-        self.search(state, solutions, n)
-        return solutions
-        
-    def is_valid_state(self, state, n):
-        return len(state) == n
 
-    def get_candidates(self, state, n):
-        if not state:
-            return range(n)
-        
-        
-        position = len(state)
-        candidates = set(range(n))
-        # prune down candidates that place the queen into attacks
-        for row, col in enumerate(state):
-            # discard the column index if it's occupied by a queen
-            candidates.discard(col)
-            dist = position - row
-            # discard diagonals
-            candidates.discard(col + dist)
-            candidates.discard(col - dist)
-        return candidates
+        def backtrack(row=0, path=[]):
+            #  Base Case
+            if row == n:
+                res.append(path[:])
+                return
 
-    def search(self, state, solutions, n):
-        if self.is_valid_state(state, n):
-            state_string = self.state_to_string(state, n)
-            solutions.append(state_string)
-            return
+                # Candidates
 
-        for candidate in self.get_candidates(state, n):
-            # recurse
-            state.append(candidate)
-            self.search(state, solutions, n)
-            state.pop()
-    
-    def state_to_string(self, state, n):
-        ret = []
-        for i in state:
-            string = '.' * i + 'Q' + '.' * (n - i - 1)
-            ret.append(string)
-        return ret
+            for col in range(n):
+                if self.isValid(row, col, path):
+                    path.append("." * col + "Q" + "." * (n - col - 1))
+                    backtrack(row + 1, path)
+                    path.pop()
+        res = []
+        backtrack()
+        return res
