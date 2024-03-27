99ab1016d90036c71b717c74b6f39600829056cc
Usmael Abdurhaman <uabdureman@gmail.com>
2024-03-27 11:10:55 +0300

    Time: 636 ms (97.09%), Space: 34.4 MB (33.33%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -4,14 +4,13 @@ class Solution:
         if valueDiff < 0:
             return False
 
-        # Create buckets based on the value difference
+    
         bucket_size = valueDiff + 1
         buckets = {}
 
         for i, num in enumerate(nums):
             bucket_index = num // bucket_size
 
-            # Check for duplicates within the same bucket or adjacent buckets
             if bucket_index in buckets:
                 return True
             if bucket_index - 1 in buckets and abs(num - buckets[bucket_index - 1]) <= valueDiff:
