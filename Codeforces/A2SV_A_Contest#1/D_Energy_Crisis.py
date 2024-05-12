fad78c4c65fad1e89e5b32fc17deee02166be413
Usmaelabdureman <uabdureman@gmail.com>
2024-05-12 17:09:36 +0300

    energy crisis

---
+++ Codeforces/A2SV_A_Contest#1/D_Energy_Crisis.py
@@ -0,0 +1,25 @@
+import sys
+n, k = map(int, sys.stdin.readline().strip().split())
+nums = list(map(int, sys.stdin.readline().strip().split()))
+
+def chekcker(x):
+    gain = 0
+    need = 0
+    for num in nums:
+        if num > x:
+            diff = num - x
+            gain += (diff - diff * k / 100)
+        if num < x:
+            need  += (x - num)
+    
+    return gain >= need
+
+low = min(nums)
+high = max(nums)
+while high  - low > 10 ** -7:
+    mid = (high + low) / 2
+    if chekcker(mid):
+        low = mid
+    else:
+        high = mid
+print(high)
