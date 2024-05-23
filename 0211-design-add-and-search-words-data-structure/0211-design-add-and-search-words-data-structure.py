fe80cec09d829b1cd1f963cc4f20367457cec312
Usmael Abdurhaman <uabdureman@gmail.com>
2024-05-23 19:31:13 +0300

    Time: 1536 ms (61.20%), Space: 64.1 MB (75.86%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,34 +1,41 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}  # dictionary mapping characters to child nodes
-        self.is_word = False  # indicates if the node represents the end of a word
+        self.children = {}  
+        self.is_end = False
+        
         
 class WordDictionary:
+    
     def __init__(self):
-        self.root = TrieNode()  # the root of the trie
+        self.root = TrieNode()  
         
     def addWord(self, word: str) -> None:
+        
         node = self.root
+        
         for char in word:
             if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-        node.is_word = True
+        node.is_end = True
         
     def search(self, word: str) -> bool:
         
-        def search_helper(node, i):
+        def dfs(node, i):
             if i == len(word):
-                return node.is_word
+                return node.is_end
+            
             char = word[i]
+            
             if char == '.':
                 for child in node.children.values():
-                    if search_helper(child, i+1):
+                    if dfs(child, i+1):
                         return True
             else:
+                
                 child = node.children.get(char)
                 if child:
-                    return search_helper(child, i+1)
+                    return dfs(child, i+1)
             return False
             
-        return search_helper(self.root, 0)
+        return dfs(self.root, 0)
