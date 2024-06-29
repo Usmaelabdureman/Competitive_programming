5dfa8c23c15e3397a5123d921ba377a2e0bbc458
Usmael Abdurhaman <uabdureman@gmail.com>
2024-06-29 10:45:43 +0300

    Time: 677 ms (76.27%), Space: 30.5 MB (8.23%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -1,40 +1,34 @@
 class Solution:
-    # Check if we can place 'm' balls at 'position'
-    # with each ball having at least 'x' gap.
-    def can_place_balls(self, x, position, m):
-        # Place the first ball at the first position.
-        prev_ball_pos = position[0]
-        balls_placed = 1
-
-        # Iterate on each 'position' and place a ball there if we can place it.
-        for i in range(1, len(position)):
-            curr_pos = position[i]
-            # Check if we can place the ball at the current position.
-            if curr_pos - prev_ball_pos >= x:
-                balls_placed += 1
-                prev_ball_pos = curr_pos
-            # If all 'm' balls are placed, return 'True'.
-            if balls_placed == m:
-                return True
-        return False
-
     def maxDistance(self, position: List[int], m: int) -> int:
-        answer = 0
+        
+        def canPlace(x,pos,m):
+            prev_ball_pos = pos[0]
+            balls_placed = 1
+            
+            for i in range(1,len(pos)):
+                curr_pos = pos[i]
+                if curr_pos - prev_ball_pos >= x:
+                    balls_placed += 1
+                    prev_ball_pos = curr_pos
+                if balls_placed == m:
+                    return True
+            return False
+
+        
         n = len(position)
         position.sort()
 
-        # Initial search space.
         low = 1
         high = int(position[-1] / (m - 1.0)) + 1
+        
         while low <= high:
             mid = low + (high - low) // 2
-            # If we can place all balls having a gap at least 'mid',
-            if self.can_place_balls(mid, position, m):
-                # then 'mid' can be our answer,
-                answer = mid
-                # and discard the left half search space.
+            
+            if canPlace(mid, position, m):
+
                 low = mid + 1
             else:
-                # Discard the right half search space.
+
                 high = mid - 1
-        return answer
+                
+        return low - 1
